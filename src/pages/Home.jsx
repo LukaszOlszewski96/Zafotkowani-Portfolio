9824ab1a453b64c1a0a6572c6f555602312daa96
LukaszOlszewski96 <lukaszolszewski96@gmail.com>
2022-03-01 00:22:38 +0100

    Add photo for home page, before create slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Container, Box } from '@mui/material'
+import React from 'react';
+import '../styles/Pages/Home/Home.css'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Container maxWidth="xl">
+        <Box sx={{height: '80vh' }}>
+          <div className='image-home'></div>
+        </Box>
+      </Container>
+    </>
   )
 }
